e8b6218587a56a2de81d8241ca9e516030e34a3d
jimhanboi <songjoehan@gmail.com>
2025-02-23 00:11:43 +0800

    Update: database connection

---
+++ conn.php
@@ -0,0 +1,12 @@
+$localhost = 'localhost';
+$user = 'root';
+$pass = "";
+$dbName = "mewfit";
+
+$dbConn = mysqli_connect($localhost,$user,$pass,$dbName);
+
+if(mysqli_connect_errno()){
+    die('<script>alert("Connection failed")</script>')
+}
+
+echo "<script>alert("Successfully connect")</script>"
