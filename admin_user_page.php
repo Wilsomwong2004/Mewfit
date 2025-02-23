08248b470de6c5f15418169461823a40c651ab3a
jimhanboi <songjoehan@gmail.com>
2025-02-23 22:18:29 +0800

    Update: admin user page but idk why I can't load mysql

--- admin_user_page.php
+++ admin_user_page.php
@@ -19,12 +19,47 @@
         </ul>
     </nav>
 
+    <h2 class="title"> USER <span>PROFILE</span></h2>
+
     <div class="container">
-        <!-- User Profile Table -->
         <div class="profile-table">
-            <h2> USER <span>PROFILE</span></h2>
             <input type="text" class="search-bar" placeholder="Search">
-                <div class="box"></div>
+            <div class="box">
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Name</th>
+                        <th>Gender</th>
+                        <th>Email Address</th>
+                        <th>Phone Number</th>
+                    </tr>
+                    <?php
+                        //display data
+                        // ini_set('display_errors', 0);
+                        include "conn.php";
+                        
+                        $sql="SELECT * FROM administrator";
+                        $result=mysqli_query($dbConn,$sql);
+                        if(mysqli_num_rows($result)<=0){
+                            die("<script>alert('No data from database')</script>");
+                        }
+
+                        while($rows = mysqli_fetch_array($result)){
+                            echo "<tr>";
+                            echo "<td>".$rows['admin_id']."</td>";
+                            echo "<td>".$rows['username']."</td>";
+                            echo "<td>".$rows['password']."</td>";
+                            echo "<td>".$rows['name']."</td>";
+                            echo "<td>".$rows['gender']."</td>";
+                            echo "<td>".$rows['email_address']."</td>";
+                            echo "<td>".$rows['phone_number']."</td>";
+                            echo "<tr>";
+                        }
+                    ?>
+                </table>
+            </div>
         </div>
 
         <!--Add New Profile Form -->
@@ -43,7 +78,7 @@
                 <input type="text" id="name" name="name" required>
 
                 <label for="gender">Gender</label>
-                <select id="gender" name="gender" required>
+                <select id="gender" name="gender" required style="width:98%;">
                     <option value="">Select Gender</option>
                     <option value="female">Female</option>
                     <option value="male">Male</option>
@@ -62,7 +97,8 @@
         </div>
     </div>
     <?php
-    ini_set('display_errors', 0);
+    
+    // enter new data
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $username = $_POST['username'];
         $password = $_POST['password'];
@@ -70,19 +106,17 @@
         $gender = $_POST['gender'];
         $email = $_POST['email'];
         $phone_num = $_POST ['phonenum'];
+
+        $sql = "INSERT INTO administrator(username, password, name, gender, email_address, phone_number) 
+        VALUES('$username','$password','$name','$gender', '$email', '$phone_num');";
+
+        if (!$dbConn->query($sql)) {
+            die("Failed to update Laptop table");
+        }
+        $dbConn->close();
+
+        echo "<script>alert('Sucessfully insert data')</script>";
     }
-
-    include "conn.php";
-
-    $sql = "INSERT INTO administrator(username, password, name, gender, email_address, phone_number) 
-    VALUES('$username','$password','$name','$gender', '$email', '$phone_num');";
-
-    if (!$dbConn->query($sql)) {
-        die("Failed to update Laptop table");
-    }
-    $dbConn->close();
-
-    echo "<script>alert('Sucessfully insert data')</script>"
 ?>
 </body>
 
