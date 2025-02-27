bca492fd846c9783e1bc8ca78305a25dc97f0c48
Janson-tired <jansun280@gmail.com>
2025-02-27 22:32:11 +0800

    Update: sign_up_page now add data to database

--- js/email-otp.js
+++ js/email-otp.js
@@ -2,11 +2,32 @@ function sendOTP() {
   const email = document.getElementById("email-verify");
   const otpVerify = document.getElementById("email-otp")[0];
 
+  let otpValue = Math.floor(Math.random() * 10000);
+
+  let emailbody = `<h2>Your OTP is </h2><p>${otpValue}</p>`;
+
   Email.send({
-    SecureToken: "C973D7AD-F097-4B95-91F4-40ABC5567812",
-    To: "them@website.com",
-    From: "you@isp.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
+    SecureToken: "903c017d-aa20-4252-ae06-9a237f4d683f",
+    To: email.value,
+    From: "TP074890@mail.apu.edu.my",
+    Subject: "MewFit Password Recovery",
+    Body: emailbody,
+  }).then((message) => {
+    if (message === "OK") {
+      alert("OTP sent to your email " + email.value);
+
+      otpVerify.style.display = "flex";
+      const otp_inp = document.getElementById("email-otp");
+      const otp_btn = document.getElementById("otp-button");
+
+      otp_btn.addEventListener("click", () => {
+        if (otp_inp.value == otpValue) {
+          alert("Email address verified");
+        } else {
+          alert("OTP invalid, Email verification failed");
+        }
+      });
+    }
+  });
 }
+
