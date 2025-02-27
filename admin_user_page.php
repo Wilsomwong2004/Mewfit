840aa5513721845126b6099c367761c6161fdeee
jimhanboi <songjoehan@gmail.com>
2025-02-27 14:07:59 +0800

    Update: errors detection code bug

--- admin_user_page.php
+++ admin_user_page.php
@@ -2,7 +2,167 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
- 9
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="css/admin_user_page.css">
+    <script src="js/admin_user_page.js" defer></script>
+</head>
+<?php
+    include "conn.php";   
+?>
+<body>
+    <nav>
+        <div class="logo-space"> <div class="logo-and-title"></div>
+            <img src="assets/icons/logo.svg" style="height:40px;" alt="Logo">
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">DASHBOARD</a></li>
+            <li><a href="#" class="active">USER</a></li>
+            <li><a href="#">WORKOUT</a></li>
+            <li><a href="#">MEALS</a></li>
+        </ul>
+    </nav>
+
+    <div style="display:flex;">
+        <h2 class="title"> USER <span>PROFILE</span></h2>
+        <ul class="user-section">
+            <li><a href="#member" class="member-link">MEMBER</a></li>
+            <li><a href="#admin" class="admin-link">ADMIN</a></li>
+        </ul>
+    </div>
+    
+    
+    <div class="content">
+        <div class="admin-container">
+            <div class="section1">
+                <input type="text" class="search-bar" placeholder="Search">
+                <div class="box">
+                    <table>
+                        <tr>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>Password</th>
+                            <th>Name</th>
+                            <th>Gender</th>
+                            <th>Email Address</th>
+                            <th>Phone Number</th>
+                        </tr>
+                        <?php
+                        $sql = "SELECT * FROM administrator";
+                        $result = mysqli_query($dbConn, $sql);
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($rows = mysqli_fetch_array($result)) {
+                                echo "<tr admin-id='".$rows['admin_id']."'>";
+                                echo "<td>".$rows['admin_id']."</td>";
+                                echo "<td>".$rows['username']."</td>";
+                                echo "<td>".$rows['password']."</td>";
+                                echo "<td>".$rows['name']."</td>";
+                                echo "<td>".$rows['gender']."</td>";
+                                echo "<td>".$rows['email_address']."</td>";
+                                echo "<td>".$rows['phone_number']."</td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                            echo "<tr class='no-data'><td colspan='7'>No data available</td></tr>";
+                            $sql="TRUNCATE TABLE administrator";
+                        }
+                        ?>
+                    </table>
+                </div>
+                <div class="table-option">
+                    <button id="edit-btn" disabled>Edit</button>
+                    <button id="delete-btn" disabled>Delete</button>
+                </div>
+            </div>
+
+            <!--Add New Profile Form -->
+            <div class="add-profile">
+                <center>
+                <h2>Add New <span>Profile</span></h2> 
+                </center>
+                <form method="post" action="">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" required>
+                    
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" required>
+
+                    <label for="gender">Gender</label>
+                    <select id="gender" name="gender" required style="width:98%;">
+                        <option value="">Select Gender</option>
+                        <option value="female">Female</option>
+                        <option value="male">Male</option>
+                    </select>
+
+                    <label for="email">Email Address</label>
+                    <input type="email" id="email" name="email" required>
+
+                    <label for="phonenum">Phone Number</label>
+                    <input type="text" id="phonenum" name="phonenum" required>
+
+                    <div style="display:flex;justify-content: flex-end;white-space: nowrap;">
+                        <button type="submit" id="add-profile-btn">Create New</button>
+                    </div>
+                </form>
+            </div>
+            <div class="edit-profile">
+                <center>
+                <h2>Edit <span>Profile</span></h2> 
+                </center>
+                <?php
+                if (!empty($errors)) {
+                    echo '<div class="error-messages">';
+                    foreach ($errors as $error) {
+                        echo "<p style='color:red;'>$error</p>";
+                    }
+                    echo '</div>';
+                    
+                }
+                ?>
+                <form method="post" action="edit.php" id="administrator">
+                <input type="hidden" id="selectedAdminId" name="selectedAdminId" value="<?php echo $selectedAdminId; echo htmlspecialchars($old_data['eusername'] ?? '');?>">
+                <input type="hidden" id="table" name="table" value="administrator">
+                    
+                <label for="eusername">Username</label>
+                <input type="text" id="eusername" name="eusername" value="<?php echo htmlspecialchars($old_data['eusername'] ?? ''); ?>" required>
+
+                    <label for="epassword">Password</label>
+                    <input type="epassword" id="epassword" name="epassword" value="<?php echo htmlspecialchars($old_data['epassword'] ?? ''); ?>" required>
+
+                    <label for="ename">Name</label>
+                    <input type="text" id="ename" name="ename" value="<?php echo htmlspecialchars($old_data['ename'] ?? ''); ?>" required>
+
+                    <label for="egender">Gender</label>
+                    <select id="egender" name="egender" required style="width:98%;">
+                        <option value="">Select Gender</option>
+                        <option value="female" <?php echo (isset($old_data['egender']) && $old_data['egender'] == 'female') ? 'selected' : ''; ?>>Female</option>
+                        <option value="male" <?php echo (isset($old_data['egender']) && $old_data['egender'] == 'male') ? 'selected' : ''; ?>>Male</option>
+                    </select>
+
+                    <label for="eemail">Email Address</label>
+                    <input type="email" id="eemail" name="eemail" value="<?php echo htmlspecialchars($old_data['eemail'] ?? ''); ?>" required>
+
+                    <label for="ephonenum">Phone Number</label>
+                    <input type="text" id="ephonenum" name="ephonenum" value="<?php echo htmlspecialchars($old_data['ephonenum'] ?? ''); ?>" required>
+
+                    <div style="display:flex;justify-content: flex-end;gap:20px;white-space: nowrap;">
+                        <button type="button" id="discard-btn">Discard Changes</button>
+                        <button type="submit" id="confirm-btn">Update Changes</button>
+                    </div>
+                </form>
+            </div>
+            <div class="popup" id="popup">
+                <div class="popup-content">
+                    <h2>Confirm Deletion</h2>
+                    <p>Are you sure you want to delete this record?</p>
+                    <button id="confirmDelete">Yes, Delete</button>
+                    <button id="cancelDelete">Cancel</button>
+                </div>
+            </div>
+        </div>
         <div class="member-container">
             <input type="text" class="search-bar" placeholder="Search">
             <div class="member-box">
