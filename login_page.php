90dbf7b7abbfca8af154670e99708726a82904e0
Janson-tired <jansun280@gmail.com>
2025-03-05 17:26:18 +0800

    Update: can now do simple login

--- login_page.php
+++ login_page.php
@@ -1,3 +1,43 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mewfit";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$exist_username = false;
+$homepage = "homepage.html";
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $sql = "SELECT * FROM member";
+  $result = $conn->query($sql);
+
+  if (!$result) {
+    die("Query failed: " . $conn->error);
+  }
+
+  while ($row = $result->fetch_assoc()) {
+    if (($row['username'] == $username) && password_verify($password, $row['password'])) {
+      $_SESSION["logged_in"] = true;
+      $_SESSION["username"] = $row['username'];
+      
+      header("Location: " . $homepage);
+      exit();
+    }
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
